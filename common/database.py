cde63b5558463edd7a6ba80d79e5273572c16041
HardbanRecordsLab <hardbanrecordslab.pl@gmail.com>
2025-08-12 22:33:45 +0200

    Fix database URL loading from .env file

--- common/database.py
+++ common/database.py
@@ -2,14 +2,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict # Dodajemy SettingsConfigDict
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str = "postgresql://user:password@postgresserver/db"
-    class Config:
-        env_file = ".env"
+    """
+    Klasa do zarządzania zmiennymi środowiskowymi.
+    """
+    sqlalchemy_database_url: str 
 
+    # Ta nowa sekcja mówi Pydantic, żeby szukał pliku .env
+    model_config = SettingsConfigDict(env_file=".env")
+
+# Tworzymy instancję naszych ustawień
 settings = Settings()
+
+# Tworzymy "silnik" bazy danych, używając adresu URL z naszej konfiguracji
 engine = create_engine(settings.sqlalchemy_database_url)
+
+# Tworzymy "fabrykę" sesji
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Tworzymy klasę bazową dla naszych modeli
 Base = declarative_base()
